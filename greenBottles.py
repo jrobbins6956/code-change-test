3ad90e336a0c8d71507e2e6a3f588459164f79ff
jrobbins6956 <jrobbins6956@sohamcollege.org.uk>
2014-05-07 09:46:34 +0100

    Update greenBottles.py
    added timings and option of how many bottles are on their wall

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,18 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+bottles = int(input("how many bottles do you have on your wall"))
+import time
+
+while bottles >= 1:
+    print()
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    time.sleep(1.5)
+    print("{0} green bottles, hanging on the wall".format(bottles))
+    time.sleep(1.5)
+    print("And if 1 green bottle should acidentally fall,")
+    time.sleep(1.5)
+    print("They'll be {0} green bottles hanging on the wall.".format(bottles-1))
+
+    bottles = bottles - 1
+
+    time.sleep(2)
+    print()
+    print()
