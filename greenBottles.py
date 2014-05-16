893942e9961f9e1df15c3127cd76223f374b8ef7
James Robinson <jrobinson@soham-college.org.uk>
2014-05-16 06:07:25 +0100

    Update greenBottles.py

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,4 @@
 print("10 green bottles, hanging on the wall")
 print("10 green bottles, hanging on the wall")
 print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+print("They'd be 9 green bottles hanging on the wall.")
